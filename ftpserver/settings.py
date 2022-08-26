0a5f959dc24d0fdf74d05a4089df29e2ffb7e181
dev-aroundborders <dev.aroundborders@gmail.com>
2022-08-27 00:24:14 +0200

    test deploy

--- ftpserver/settings.py
+++ ftpserver/settings.py
@@ -134,6 +134,9 @@ STATICFILES_DIRS = [
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
